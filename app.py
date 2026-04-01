3c88a8e82b7b89044cce28eb0fc1ac23441c9305
DarioN370 <netobis2015@gmail.com>
2026-04-01 14:01:54 -0300

    Fix: Atualizei a conexão automatica, agora ela permite acessar externamente o link, usei isso para configurar um link externo com o ngrok

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ if __name__ == '__main__':
     inicializar_banco()
     
     print(f"🚀 Servidor em http://127.0.0.1:5000/promocao")
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
